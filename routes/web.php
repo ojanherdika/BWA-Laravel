8f332a38fe0ca6305c4b0e9c944858ea0025fb36
ojan <ojanherdika@gmail.com>
2020-02-22 13:31:43 +0700

    add admin page (udah di rapihin)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('admin')->namespace('Admin')->group(function(){
+Route::get('/','DashboardController@index')->name('dashboard');
+});
